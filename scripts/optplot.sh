891f2d215f8208117db05789ecf7e8b48f3cd9a0
ftrimble <trimble.forest@gmail.com>
2013-05-04 19:56:24 -0400

    more work on the write-up

---
+++ scripts/optplot.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# This script takes in an opt tour file and a coordinate file 
+# and an output file name and outputs plottable coordinates for
+# the path into the output file
+
+# Aliases for args
+pathfile=$1
+coordsfile=$2
+outfile=$3
+
+# Start anew
+rm $outfile
+
+# parse path file
+i=0
+while read line; do
+    if [[ $i == 3 ]]; then
+	read -a dims <<<$line
+	m=${dims[2]}
+    fi
+    if [[ $i -gt 4 ]]; then
+	if [[ $line == -1 ]]; then
+	    break
+	fi
+	path[$i-5]=$line
+    fi
+    ((i++))
+done < $pathfile
+
+# parse coords file
+i=0
+while read line; do
+    if [[ $i -ge 6 ]]; then
+	read -a coords <<<$line
+	x[${coords[0]}]=${coords[1]}
+	y[${coords[0]}]=${coords[2]}
+    fi
+    if [[ `echo $i-$m | bc` -gt 6 ]]; then
+	break
+    fi
+    ((i++))
+done < $coordsfile
+
+# output plottable data
+i=0
+while [[ $i -lt $m ]]; do
+    echo ${x[${path[$i]}]} ${y[${path[$i]}]} >> $outfile
+    ((i++))
+done
+echo ${x[${path[0]}]} ${y[${path[0]}]} >> $outfile
---
+++ scripts/plot.p
@@ -0,0 +1,9 @@
+set terminal post eps
+
+set border 0
+set output 'plots/pr2392.eps'
+set xlabel ''
+set ylabel ''
+unset ytics
+unset xtics
+plot 'plots/pr2392.dat' w l title ''
